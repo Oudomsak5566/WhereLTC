3bc83b898754113c900791c2480d2296f1b840fd
Test <Test@email.com>
2016-12-13 14:47:11 +0700

    Link Form

--- app/src/main/java/com/example/hp/whereltc/MainActivity.java
+++ app/src/main/java/com/example/hp/whereltc/MainActivity.java
@@ -4,13 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Button mBtn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /*
+        mBtn1  = (Button) findViewById(R.id.button2);
+        mBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+        */
     } // Main method
 
     public void clickSingUpMain(View view) {
