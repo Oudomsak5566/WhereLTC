af5f84b0c3359fbae909eca5eea131338b33fc10
Test <Test@email.com>
2016-12-13 16:38:52 +0700

    Click Sign up sign

--- app/src/main/java/com/example/hp/whereltc/SignUpActivity.java
+++ app/src/main/java/com/example/hp/whereltc/SignUpActivity.java
@@ -2,12 +2,33 @@ package com.example.hp.whereltc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText1);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
+
+    } //main
+
+    public void clickSignUpSign(View view) {
+
+
     }
-}
+
+
+}//main class
