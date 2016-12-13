e5cd3aa740ea53f683a6d13ecc2dd76a45df8189
Test <Test@email.com>
2016-12-13 14:36:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hp/whereltc/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hp.whereltc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    } // Main method
+
+    public void clickSingUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    }
+
+} // Main class
