4bc19bbaa637114804e553f7b7e05a1224d7ace4
Test <Test@email.com>
2016-12-16 17:40:39 +0700

    Finish Project

--- app/src/main/java/com/example/hp/whereltc/SplashScreen.java
+++ app/src/main/java/com/example/hp/whereltc/SplashScreen.java
@@ -1,5 +1,8 @@
 package com.example.hp.whereltc;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,26 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+
+        //Sound Effect
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_tata);
+        mediaPlayer.start();
+
+
+
+        //Post Delay
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },5000);
+
+
+    }//Main Method
+
+
+}//Main Class
